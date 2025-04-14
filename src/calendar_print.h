6e6ec028dbb42576caea63fb8662179b43f69982
Roman <roman.069@mail.ru>
2025-04-14 22:48:10 +0400

    fix after review(+constexpr, getDayOfWeek())

--- src/calendar_print.h
+++ src/calendar_print.h
@@ -1,16 +1,19 @@
 #pragma once
 
+#include <array>
+#include <chrono>
 #include <ctime>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <string_view>
 
 std::string PrintMonth(int year, int month);
 
-bool isLeapYear(int year);
+constexpr bool isLeapYear(int year);
 
-int getDaysInMonth(int year, int month);
+constexpr int getDaysInMonth(int year, int month);
 
 // Возвращает день недели (0 = понедельник, 6 = воскресенье)
-int getStartDayOfWeekMondayFirst(int year, int month);
+int getDayOfWeek(int year, int month, int day = 1);
